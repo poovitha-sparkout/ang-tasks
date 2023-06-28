eccc1b6a026299b08939c7827065bae87da04402
poovitha-sparkout <poovitha@sparkouttech.com>
2023-06-28 11:06:42 +0530

    changes

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,33 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
+export class FoodListComponent  {
+  food !: {
+    name: string;
+    price: number;
+    description : string;
+    quantity : number
+  }
   products: Array<{
     name: string;
     price: number;
-  }> = [];
+    description : string;
+    quantity : number
+  }> = [
+    {
+    name : 'Non Veg Meals',
+    price: 100,
+    description : 'Food',
+    quantity : 0,
 
- 
+  },
+    {
+    name : 'Non Veg Meals',
+    price: 100,
+    description : 'Food',
+    quantity : 0,
+
+  },
+    {
+    name : 'Non Veg Meals',
+    price: 100,
+    description : 'Food',
+    quantity : 0,
+
+  },
+    {
+    name : 'Non Veg Meals',
+    price: 100,
+    description : 'Food',
+    quantity : 0,
+
+  },
+];
+
 
   constructor() { }
-  ngOnInit(): void {
-   
-  }
 
 
 
   openMenu:boolean=false;
   Payment:boolean=false;
   value = 0;
-  handleMinus() {
-    this.value--;  
+  handleMinus(food:any) {
+    // this.value--;  
+    this.products = this.products.map((product) => {
+      if (product === food) {
+        product.quantity--
+      }
+      return product;
+    });
   }
-  handlePlus() {
-    this.value++;    
+  handlePlus(food:any) {
+    this.products = this.products.map((product) => {
+      if (product === food) {
+        product.quantity++
+      }
+      return product;
+    });
+    
   }
   openPayment(){
     this.Payment = !this.Payment;
